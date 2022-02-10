7acf835776bbca0aad86c74f9a8fd54f0aa93ddb
beetee17 <brandon.thio@yahoo.com.sg>
2022-02-10 11:10:05 +0800

    Initial Commit

---
+++ Geodle/GeodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeodleApp.swift
+//  Geodle
+//
+//  Created by Brandon Thio on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GeodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
